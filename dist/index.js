5b9e0d1632ca2bf20fcc913ab712e2262368804b
blurbeast <leumasre@gmail.com>
2025-05-07 19:00:00 +0100

    kitted

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(1234);
+const addNumber = (a, b) => a + b;
+// fs.readFile()
+const resilt = addNumber(12, 4);
+console.log('result is :::', resilt);
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+import fs from 'fs';
+console.log(1234);
+
+const addNumber = (a: number, b: number) => a + b;
+
+// fs.readFile()
+
+
+const resilt: number = addNumber(12, 4);
+
+console.log('result is :::', resilt);
