fb3968fff302ffdcc7e4af0291568516f1578186
blurbeast <leumasre@gmail.com>
2025-05-08 20:02:29 +0100

    player controller and routes done

--- src/player/controller/player.controller.ts
+++ src/player/controller/player.controller.ts
@@ -1,10 +1,20 @@
+import { Request, Response } from "express";
 import { PlayerService } from "../service/player.service";
-
-
 
 export class PlayerController {
     private readonly playerService: PlayerService;
     constructor() {
         this.playerService = new PlayerService();
+    }
+
+    async createPlayer(req: Request, res: Response) {
+        try{
+            const result = await this.playerService.createPlayer(req.body);
+            res.status(201).json(result);
+        }catch(error) {
+            res.status(400).json({
+                error: error instanceof Error ? error.message : ' could not figure the problem'
+            });
+        }
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,23 @@
-import express, { Router } from "express";
+import express, { Request, Response, Router } from "express";
 import { permissionlessController } from "./permissionless/permissionless.controller";
+import { PlayerController } from "./player/controller/player.controller";
 
 
 export class AppRouter {
 
     private readonly router: Router;
     private readonly permissionlessController: permissionlessController;
+    private readonly playerController: PlayerController;
 
     constructor() {
         this.router = express.Router();
         this.permissionlessController = new permissionlessController();
+        this.playerController = new PlayerController();
     }
 
     setRouter() {
-
         this.router.get('/smart/:index', (req, res) => this.permissionlessController.createSmartAccount(req, res));
+        this.router.post('/player', (req: Request, res: Response) => this.playerController.createPlayer(req, res));
     }
 
     getRouter() {
