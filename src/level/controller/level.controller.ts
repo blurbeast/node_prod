08c1319f6a5ea546239df0da09752d381c9e9203
blurbeast <leumasre@gmail.com>
2025-05-10 16:30:09 +0100

    level controller initallized in the routes

--- src/level/controller/level.controller.ts
+++ src/level/controller/level.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { LevelService } from "../service/level.service";
 
 
@@ -11,6 +11,19 @@ export class LevelController {
         this.levelService = new LevelService();
     }
 
-    
+    async addPlayerScore(req: Request, res: Response) {
+        try{
+            const username = req.params.username;
+            const score = req.params.score;
+        
+            const result = await this.levelService.addPlayerScore(username, Number(score));
 
+            res.status(200).json(result);
+
+        }catch(error) {
+            const err = error instanceof Error ?
+             error.message : 'level controller: could not determine the error ';
+            res.status(401).json(err);
+        }
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,32 @@
 import express, { Request, Response, Router } from "express";
-import { permissionlessController } from "./permissionless/permissionless.controller";
+// import { permissionlessController } from "./permissionless/permissionless.controller";
 import { PlayerController } from "./player/controller/player.controller";
+import { LevelController } from "./level/controller/level.controller";
 
 
 export class AppRouter {
 
     private readonly router: Router;
-    private readonly permissionlessController: permissionlessController;
+    // private readonly permissionlessController: permissionlessController;
     private readonly playerController: PlayerController;
+    private readonly levelController: LevelController;
 
     constructor() {
         this.router = express.Router();
-        this.permissionlessController = new permissionlessController();
+        // this.permissionlessController = new permissionlessController();
         this.playerController = new PlayerController();
+        this.levelController = new LevelController();
         this.setRouter();
     }
 
     setRouter() {
-        this.router.get('/smart/:index', (req, res) => this.permissionlessController.createSmartAccount(req, res));
+        // this.router.get('/smart/:index', (req, res) => this.permissionlessController.createSmartAccount(req, res));
+        // player
         this.router.post('/player', (req: Request, res: Response) => this.playerController.createPlayer(req, res));
         this.router.get('/player', (req: Request, res: Response) => this.playerController.getPlayer(req, res));
+
+        // level
+        this.router.post('/player/:username/:score', (req: Request, res: Response) => this.levelController.addPlayerScore(req, res));
     }
 
     getRouter() {
