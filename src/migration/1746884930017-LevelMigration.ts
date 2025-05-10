2589e8ee672cfb95d5db32ecbd7d7f799a0e193f
blurbeast <leumasre@gmail.com>
2025-05-10 14:49:54 +0100

    level migration

---
+++ src/migration/1746884930017-LevelMigration.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class LevelMigration1746884930017 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+         await queryRunner.createTable(
+                    new Table({
+                        name: 'level',
+                        columns: [
+                            {
+                                name: 'id',
+                                isPrimary: true,
+                                type: 'int',
+                                isGenerated: true,
+                                isUnique: true,
+                                generationStrategy: 'increment'
+                            },
+                            {
+                                name: 'player_user_name',
+                                type: 'varchar',
+                                isUnique: true,
+                                isNullable: false
+                            },
+                            {
+                                name: 'player_score',
+                                type: 'int',
+                                default: 0
+                            }
+                        ]
+                    })
+                )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
