7a7b9b8bd7710633ede137e24161b80c67461d68
blurbeast <leumasre@gmail.com>
2025-05-10 12:44:31 +0100

    created the level entity migration and files

---
+++ src/level/entity/level.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+
+
+
+@Entity()
+export class Level {
+    @PrimaryGeneratedColumn('increment')
+    id!: number;   
+    @Column({
+        name: 'player_user_name',
+        type: 'varchar',
+        nullable: false,
+        unique: true,
+      })
+    playerUsername!: string;
+    @Column({ name: 'player_score', type: 'int', default: 0 })
+    @Index()
+    playerScore!: number;
+}
